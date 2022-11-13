72d3c939f22d3c906107c49da54810da2ae15717
Scott Taylor <sgtaylor16@gmail.com>
2022-11-13 18:39:18 -0500

    commit init

---
+++ __init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  3 18:20:52 2018
+
+@author: sgtay
+
+
+""" 
+from dinero.dinero2 import *
+from dinero.schwab import *
+from dinero.fidelity import *
+from dinero.account import *
