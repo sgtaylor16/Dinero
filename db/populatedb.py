d74d16a17e41d477b3e3aa96d80753b89b60e372
Scott Taylor <sgtaylor16@gmail.com>
2025-09-01 17:46:54 -0400

    Accounts populated

--- db/populatedb.py
+++ db/populatedb.py
@@ -7,14 +7,14 @@ import json
 # Create the session
 engine = create_engine("sqlite:///investments.db", echo=True)
 # Create the investment types
-invtypes = ['Large Cap','Small Cap','Bond','REIT','Cash','International','Unknown']
+invtypes = ['Large Cap','Small Cap','Bond','REIT','Cash','International','Unknown','Emerging Markets']
 with Session(engine) as session:
     for it in invtypes:
         newtype = InvestmentType(name=it)
         session.add(newtype)
     session.commit()
 
-
+# Populate the investments table from JSON files
 for records in os.listdir('/Users/scotttaylor/Library/CloudStorage/OneDrive-Personal/Finance/Stocks'):
     if records.endswith('.json'):
         filepath = os.path.join('/Users/scotttaylor/Library/CloudStorage/OneDrive-Personal/Finance/Stocks', records)
@@ -30,9 +30,79 @@ for records in os.listdir('/Users/scotttaylor/Library/CloudStorage/OneDrive-Pers
                     session.add(inv)
                     session.commit()
 
-#Get list of all tickers in database
+#Manually assign categories to investments
+catdict = {
+        "IJR": 2,
+        "SCHE": 8,
+        "SCHZ": 3,
+        "SCHB": 1,
+        "SCHD": 1,
+        "SCHG" : 1,
+        "SCHVB": 1,
+        "SCHP": 3,
+        "VCIT": 3,
+        "VNQ": 4,
+        "VB": 2,
+        "NOTIX": 3,
+        "JAVTX": 2,
+        "MGIAX": 6,
+        "NOSGX":2,
+        "NHMAX":3,
+        "VTMGX": 6,
+        "VBILX": 3,
+        "VTCLX": 1,
+        "VETAX": 1,
+        "WBIGX": 6,
+        "SWVXX": 5,
+        "SCHH": 4,
+        "SCHF": 6,
+        "SCHX": 1,
+        "SCHA": 2,
+        "SPTL": 3,
+        "FNBGX": 3,
+        "ITOT": 1,
+        "TIP": 3,
+        "FXAIX": 1,
+        "FXNAX": 3,
+        "FDRXX**":5,
+        "FSPSX": 6,
+        "VTI": 1,
+        "PLFIX": 7,
+        "JAMCX": 7,
+        "VBIIX": 3,
+        "FEMKX":8,
+        "VBTIX":3,
+        "VINIX":1,
+        "SPDW":6,
+        "SNVXX":3,
+        "FREL":4,
+        "MDYG":7,
+        "FLBAX":7,
+        "FLBIX":3,
+        "FSIIX":6,
+        "IWM":1,
+        "Principal":7
+    }
+
+for ticker, category in catdict.items():
+    with Session(engine) as session:
+        inv = session.execute(select(Investment).where(Investment.ticker == ticker)).scalar_one_or_none()
+        if inv:
+            inv.type_id = category
+            session.commit()
+
+#Manually create accounts
+schwabmain = Account(name="Schwab",description="Taxable Schwab Account")
+schwabira = Account(name="Schwab IRA",description="Scott Schwab IRA Account")
+rr401k = Account(name="RR 401K",description="RR 401K Account")
+schwab_managed = Account(name="Schwab Managed",description="Schwab Managed Bond Account")
+fidelity_ira = Account(name="Fidelity Roth",description="Brandy Fidelity Roth Account")
+fidelity_ira2 = Account(name="Fidelity Traditional",description="Brandy Fidelity Traditional Account")
+brandy401k = Account(name="IPX 401K",description="Brandy Empower Account")
+
 with Session(engine) as session:
-    stmt = select(Investment.ticker)
-    all_tickers = session.execute(stmt).scalars().all()
+    session.add_all([schwabmain, schwabira, rr401k, schwab_managed, fidelity_ira, fidelity_ira2, brandy401k])
+    session.commit()
+
 
 
