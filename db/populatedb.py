4b607fd6680e920a26a4d6f9010346160e199cc9
Scott Taylor <sgtaylor16@gmail.com>
2025-09-01 08:34:49 -0400

    investments populate working

--- db/populatedb.py
+++ db/populatedb.py
@@ -1,5 +1,5 @@
 from models import InvestmentType, Investment, Account
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
 import os
 import json
@@ -13,5 +13,21 @@ with Session(engine) as session:
         newtype = InvestmentType(name=it)
         session.add(newtype)
     session.commit()
+
+
+for records in os.listdir('/Users/scotttaylor/Library/CloudStorage/OneDrive-Personal/Finance/Stocks'):
+    if records.endswith('.json'):
+        filepath = os.path.join('/Users/scotttaylor/Library/CloudStorage/OneDrive-Personal/Finance/Stocks', records)
+        with open(filepath, 'r') as f:
+            data_dict = json.load(f)
+        for obj in data_dict:
+            with Session(engine) as session:
+                #Get unique values of Investment tickers
+                stmt = select(Investment.ticker).distinct()
+                unique_tickers = session.execute(stmt).scalars().all()
+                if obj['Ticker'] not in unique_tickers:
+                    inv = Investment(type_id=7, ticker=obj['Ticker'])
+                    session.add(inv)
+                    session.commit()
 
 
